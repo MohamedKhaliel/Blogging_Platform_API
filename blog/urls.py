7a765395654e43b2481f236d80510226e5c97ebf
mohamed khalil <mohamedahmed201131@gmail.com>
2024-12-19 01:37:28 +0200

    Blog_Platform

--- blog/urls.py
+++ blog/urls.py
@@ -7,13 +7,3 @@ urlpatterns = [
     path('update/<int:id>/', BlogUpdateView.as_view(), name='update'),
     path('delete/<int:id>/', BlogDeleteView.as_view(), name='delete'),
 ]
-
-# from django.urls import path
-# from .views import BlogCreationView , BlogDetailsView , BlogUpdateView , BlogDeleteView
-
-# urlpatterns = [
-#     path('create/', BlogCreationView.as_view(), name='create'),
-#     path('details/<int:id>/', BlogDetailsView.as_view(), name='details'),
-#     path('update/<int:id>/', BlogUpdateView.as_view(), name='update'),
-#     path('delete/<int:id>/', BlogDeleteView.as_view(), name='delete'),  
-# ]
